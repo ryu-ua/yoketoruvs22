dc1e55026000dbfaf47b16e7a8df377fa0970dc4
A601-01 <tnk.dat.class@gmail.com>
2022-07-07 16:05:05 +0900

    画面デザイン

--- yoketoruvs22/Program.cs
+++ yoketoruvs22/Program.cs
@@ -17,7 +17,7 @@ namespace yoketoruvs22
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ‚æ‚¯‚Æ‚é2022());
         }
     }
 }
