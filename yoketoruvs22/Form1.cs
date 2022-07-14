02311b98df3d3ec888df0f7cf9141cb99c411240
A601-01 <tnk.dat.class@gmail.com>
2022-07-14 15:17:48 +0900

    キャラ敵アイテム

--- yoketoruvs22/Form1.cs
+++ yoketoruvs22/Form1.cs
@@ -14,7 +14,23 @@ namespace yoketoruvs22
     public partial class よけとる2022 : Form
     {
         const bool isDebug = true;
-        
+
+        const int PlayerMax = 1;
+        const int EnemyMax = 10;
+        const int ItemMax = 10;
+        const int ChrMax = PlayerMax + EnemyMax + ItemMax;
+        Label[] chrs = new Label[ChrMax];
+        const int PlayerIndex = 0;
+        const int EnemyIndex = PlayerIndex + PlayerMax;
+        const int ItemIndex = EnemyIndex + EnemyMax;
+
+        const string PlayerText= "('m')";
+        const string EnemyText = "▼";
+        const string ItemText = "★";
+
+        static Random rand = new Random();
+
+
         enum State
         {
             None=-1,
