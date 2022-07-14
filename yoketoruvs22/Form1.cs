2e9ee192b4458d0cfe2bb000adde1452ac636d38
A601-01 <tnk.dat.class@gmail.com>
2022-07-14 15:24:48 +0900

    プレイヤー

--- yoketoruvs22/Form1.cs
+++ yoketoruvs22/Form1.cs
@@ -50,6 +50,25 @@ namespace yoketoruvs22
         public よけとる2022()
         {
             InitializeComponent();
+
+            for(int i=0;i<ChrMax;i++)
+            {
+                chrs[i] = new Label();
+                chrs[i].AutoSize = true;
+                if(i==PlayerIndex)
+                {
+                    chrs[i].Text = PlayerText;
+                }
+                else if(i<EnemyIndex)
+                {
+                    chrs[i].Text = EnemyText;
+                }
+                else
+                {
+                    chrs[i].Text = ItemText;
+                }
+                Controls.Add(chrs[i]);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
